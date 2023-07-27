16f08706488803c3930668e98016f1c27981e97e
BizarrIlliada <kazkoviy@icloud.com>
2023-07-28 02:57:06 +0300

    scoped slots with new file)

---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :goal="goal" secondProp="!!!"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'CourseGoals',
+
+    data() {
+      return {
+        goals: ['Finish the course', 'Become a Vue master'],
+      }
+    },
+  }
+</script>
